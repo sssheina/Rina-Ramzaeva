ce0db78ed6ad7fb7caf6a532cf8197fd5662f3f0
Veta Sheina <sssheina@gmail.com>
2023-08-20 11:01:34 +0300

    button hover

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -156,3 +156,18 @@ window.addEventListener("scroll", function() {
 });
 
 
+// __________________________BUTTON____________________________
+
+(function setGlowEffectRx() {
+  const glowEffects = document.querySelectorAll(".glow-effect");
+
+  glowEffects.forEach((glowEffect) => {
+    const glowLines = glowEffect.querySelectorAll("rect");
+    const rx = getComputedStyle(glowEffect).borderRadius;
+
+    glowLines.forEach((line) => {
+      line.setAttribute("rx", rx);
+    });
+  });
+})();
+
